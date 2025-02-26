5d70661de7033e24c04bf2d53f6b941249264ae9
your name <dalalgurdeep013@gmail.com>
2025-02-26 23:44:30 +0530

    feb commit

--- helo.js
+++ helo.js
@@ -1,1 +1,10 @@
-console.log("hllo")
+// console.log("hllo")
+// let a=10;
+// console.log("++a",++a)
+// console.log("a++",a++)
+// console.log("--a",--a)
+// console.log("a--",a--)
+// console.log("a",a)
+// console.log("a--",a--)
+
+let age=require("enter your age")
