1b0e389196216c0bfe132ee44f7b1c368c718d49
your name <dalalgurdeep013@gmail.com>
2025-02-28 22:52:09 +0530

    Added .gitignore to ignore node_modules

--- helo.js
+++ helo.js
@@ -6,6 +6,8 @@
 // console.log("a--",a--)
 // console.log("a",a)
 // console.log("a--",a--)
+const prompt=require("prompt-sync")();
 
-
-let age=require("enter your age")
+var a=prompt("enter 1st no")
+var b=prompt("enter 1st no")
+console.log(a+b)
